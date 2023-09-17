c0d7072e12933f04d03e5d25933a860c4fc3c544
you-itechtools <145273168+you-itechtools@users.noreply.github.com>
2023-09-17 11:21:21 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div>
+        <h1>Image Generation By AI</h1>
+    </div>
+    <div>
+        <form action="image-ai.php" method="post">
+            <div>
+                <input type="text" name="prompt" placeholder="3D Game Characters" />
+            </div>
+            <br>
+            <div>
+                <input type="submit" value="Generate Image" />
+            </div>
+        </form>
+    </div>
+</body>
+</html>
